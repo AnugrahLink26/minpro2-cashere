eaa54c2754a5f8a3d8b2a6e056eb7b237fd7d087
luthfialfinnegan <gatesluthfi@gmail.com>
2023-11-13 09:41:26 +0700

    ganti branch

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,13 @@ import { useDispatch } from "react-redux";
 import axios from "axios";
 import { setData } from "./redux/userSlice";
 import { Box } from "@chakra-ui/react";
+import { SettingsPage } from "./pages/settingPage";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/settings", element: <SettingsPage /> },
+]);
 
 function App() {
   const token = localStorage.getItem("token");
@@ -29,7 +36,7 @@ function App() {
   return (
     <div>
       <Box bg={"#F1F3F4"}>
-        <HomePage />
+        <RouterProvider router={router} />
       </Box>
     </div>
   );
--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -1,21 +1,39 @@
-import { Box, Button, Container, Flex, Grid, GridItem, Spacer, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Grid,
+  Spacer,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
-
+import { motion } from "framer-motion";
 import { FiHome, FiSettings, FiLogOut } from "react-icons/fi";
 import { VscGraph } from "react-icons/vsc";
 
 import logo1 from "../assets/logo1.png";
 
 export const Sidebar = () => {
-  const iconSize = useBreakpointValue({ base: '0px', sm: '20px', md: '30px', lg: '40px', xl: '50px' })
-  const iconBoxSize = useBreakpointValue({base: "0px", sm: "80px", md: "90px", lg: "100px", xl: "110px"})
+  const iconSize = useBreakpointValue({
+    base: "0px",
+    sm: "20px",
+    md: "30px",
+    lg: "40px",
+    xl: "50px",
+  });
+  const iconBoxSize = useBreakpointValue({
+    base: "0px",
+    sm: "80px",
+    md: "90px",
+    lg: "100px",
+    xl: "110px",
+  });
 
   const menuList = [
-    { 'icon': <FiHome fontSize={iconSize} />, 'name':'Home' },
-    { 'icon': <VscGraph fontSize={iconSize} />, 'name':'Report' },
-    { 'icon': <FiSettings fontSize={iconSize} />, 'name':'Setting' },
-    
-  ]
+    { icon: <FiHome fontSize={iconSize} />, name: "Home" },
+    { icon: <VscGraph fontSize={iconSize} />, name: "Report" },
+    { icon: <FiSettings fontSize={iconSize} />, name: "Setting" },
+  ];
 
   const handleLogOut = async () => {
     try {
@@ -26,9 +44,20 @@ export const Sidebar = () => {
     }
   };
   return (
-    <Grid bg={"white"} h={"100vh"}>
-      <Flex direction={'column'}>
-        <Image src={logo1} marginX={"auto"} my={"12.5px"} /> 
+    <Grid bg={"white"} h={"100vh"} borderRight={"1px"} borderColor={"gray.200"}>
+      <Flex direction={"column"}>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Image
+            src={logo1}
+            pr={{ md: "10px", lg: "10px" }}
+            marginX={"auto"}
+            my={"12.5px"}
+          />
+        </motion.div>
         <Container
           h={"50vh"}
           mt={"50px"}
@@ -36,60 +65,91 @@ export const Sidebar = () => {
           flexDirection={"column"}
           justifyContent={"space-between"}
         >
-          {menuList.map((item) => (
-            <Box
-              rowGap={"0.5"}
-              display={"flex"}
-              flexDirection={"column"}
-              alignItems={"center"}
-              cursor={"pointer"}
+          {menuList.map((item, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{
+                duration: 0.5,
+                delay: index * 0.1,
+                ease: "easeInOut",
+              }}
             >
               <Box
-                w={iconBoxSize}
-                py={"10px"}
-                _hover={{
-                  bg: "#ED0A72",
-                  color: "white",
-                  transitionDuration: "0.5s",
-                }}
-                rounded={"xl"}
+                rowGap={"0.5"}
                 display={"flex"}
-                rowGap={"1"}
                 flexDirection={"column"}
                 alignItems={"center"}
+                cursor={"pointer"}
+                w={{ md: "116px", lg: "137px" }}
+                bg={"white"}
+                roundedLeft={"xl"}
+                // F1F3F4
+                _hover={{ bg: "#F1F3F4", transitionDuration: "0.5s" }}
               >
-                {item.icon}
-                {item.name}
+                <Box
+                  w={iconBoxSize}
+                  my={"10px"}
+                  py={"10px"}
+                  _Focus={{
+                    bg: "red",
+                  }}
+                  _hover={{
+                    bg: "#ED0A72",
+                    color: "white",
+                    transitionDuration: "0.5s",
+                  }}
+                  rounded={"xl"}
+                  display={"flex"}
+                  rowGap={"1"}
+                  flexDirection={"column"}
+                  alignItems={"center"}
+                  pr={{ md: "10px", lg: "10px" }}
+                >
+                  {item.icon}
+                  {item.name}
+                </Box>
               </Box>
-            </Box>
+            </motion.div>
           ))}
         </Container>
         <Spacer />
-        <Box
-          display={"flex"}
-          flexDirection={"column"}
-          alignItems={"center"}
-          cursor={"pointer"}
+        <motion.div
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{
+            duration: 0.5,
+            delay: menuList.length * 0.1,
+            ease: "easeInOut",
+          }}
         >
           <Box
-            w={iconBoxSize}
-            py={"10px"}
-            _hover={{
-              bg: "#ED0A72",
-              color: "white",
-              transitionDuration: "0.5s",
-            }}
-            rounded={"xl"}
             display={"flex"}
-            rowGap={"1"}
             flexDirection={"column"}
             alignItems={"center"}
-            mb={"20px"}
+            cursor={"pointer"}
           >
-            <FiLogOut onClick={handleLogOut} fontSize={iconSize} />
-            Log Out
+            <Box
+              w={iconBoxSize}
+              py={"10px"}
+              _hover={{
+                bg: "#ED0A72",
+                color: "white",
+                transitionDuration: "0.5s",
+              }}
+              rounded={"xl"}
+              display={"flex"}
+              rowGap={"1"}
+              flexDirection={"column"}
+              alignItems={"center"}
+              mb={"20px"}
+            >
+              <FiLogOut onClick={handleLogOut} fontSize={iconSize} />
+              Log Out
+            </Box>
           </Box>
-        </Box>
+        </motion.div>
       </Flex>
     </Grid>
   );
